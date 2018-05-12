5488124673f6a7350266ad26a5ba247f689e2d7d
ExcaliburXX <e.ngo@student.unsw.edu.au>
2018-05-13 01:42:55 +1000

    removed old debugging lines

--- GUI/Grid.java
+++ GUI/Grid.java
@@ -4,13 +4,11 @@ import javafx.stage.Screen;
 import javafx.geometry.Rectangle2D;
 import java.util.ArrayList;
 import javafx.scene.paint.Color;
-//import javafx.scene.paint.Paint;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.geometry.Bounds;
 
 public class Grid {
 
@@ -29,11 +27,9 @@ public class Grid {
 	private double orgTranslateY;
 	private int gridLength;
 	private int sLength;
-	//private Group m_draggableNode;
 
-	// can change to one for loop . size*size, if i % size == 0, make xPos = 0, yPos += 50
+
 	public Grid(int size){
-		//Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		this.sLength = 50;
 		this.gridLength = size*sLength;
 		double xPos = 0;
@@ -52,17 +48,13 @@ public class Grid {
 	}
 
 	public Group createSprite(int xPos, int yPos, int w, int h){
-		System.out.println("Wat");
 		Group dragNode = new Group();
-		//double orgSceneX, orgSceneY;
 		Sprite s = new Sprite(xPos, yPos, w, h);
 		dragNode.getChildren().add(s);
 		dragNode.setOnMousePressed(rectOnMousePressedEventHandler);
 		dragNode.setOnMouseDragged(rectOnMouseDraggedEventHandler);
 		this.blocks.add(s);
 		return dragNode;
-		// ImageView i = new ImageView(s.getSprite());
-		// return i; - might have forums for custom objects added scene like this one
 	}
 
 	public ArrayList<Rectangle> getGridSquares(){
@@ -74,8 +66,6 @@ public class Grid {
 
 		@Override
 		public void handle(MouseEvent t) {
-			// System.out.println(gridLength);
-			//System.out.println(t.getSource().getClass().getName() + " | " + ((Group)t.getSource()).getChildren().get(0).getTranslateX());
 			orgSceneX = t.getSceneX();
 			orgSceneY = t.getSceneY();
 			orgTranslateX = ((Group)t.getSource()).getChildren().get(0).getTranslateX();
@@ -88,15 +78,12 @@ public class Grid {
 
 		@Override
 		public void handle(MouseEvent t) {
-			//System.out.println("gridLength" + gridLength);
-			Sprite block = ((Sprite)((Group)t.getSource()).getChildren().get(0));
-			if (block.getOrientation().equals("HORIZONTAL")){
+			Sprite block = ((Sprite)((Group)t.getSource()).getChildren().get(0)); // block that is currently being dragged
+			if (block.getOrientation().equals("HORIZONTAL")){ // IF BLOCK IS HORIZONTAL
 
 				double offsetX = t.getSceneX() - orgSceneX;
 				double newTranslateX = orgTranslateX + offsetX;
-				// System.out.println("NEWTRX: " + newTranslateX + " | X: " + block.getX());
-				// System.out.println(ANSI_BLUE + "\t[W]" + block.getWidth() + " | " + ANSI_RED + (newTranslateX + block.getWidth()) + ANSI_RESET);
-				if (block.getX() + newTranslateX < 0){
+				if (block.getX() + newTranslateX < 0){ // if going out of left end of grid
 					if (block.getX() == 0){
 						newTranslateX = 0;
 					}
@@ -104,18 +91,15 @@ public class Grid {
 						newTranslateX += Math.abs(newTranslateX + block.getX());
 					}
 				}
-				else if ((block.getX() + newTranslateX + block.getWidth()) > gridLength){
-					System.out.println(ANSI_GREEN + "..................." + ANSI_RESET);
+				else if ((block.getX() + newTranslateX + block.getWidth()) > gridLength){ // if going out of right end of grid
 					newTranslateX -= (block.getX() + newTranslateX + block.getWidth()) - gridLength;
 				}
 				((Group)t.getSource()).getChildren().get(0).setTranslateX(newTranslateX);
 			}
-			else {
+			else { // IF BLOCK IS VERTICAL
 				double offsetY = t.getSceneY() - orgSceneY;
 				double newTranslateY = orgTranslateY + offsetY;
-				// System.out.println("NEWTRY: " + newTranslateY + " | Y: " + block.getY());
-				// System.out.println(ANSI_BLUE + "\t[W]" + block.getHeight() + " | " + ANSI_RED + (newTranslateY + block.getHeight()) + ANSI_RESET);
-				if ((block.getY() + newTranslateY) < 0){
+				if ((block.getY() + newTranslateY) < 0){ // if going out of left end of grid
 					if (block.getY() == 0){
 						newTranslateY = 0;
 					}
@@ -123,25 +107,13 @@ public class Grid {
 						newTranslateY += Math.abs(newTranslateY + block.getY());
 					}
 				}
-				else if ((block.getY() + newTranslateY + block.getHeight()) > gridLength){
-					System.out.println(ANSI_GREEN + "..................." + ANSI_RESET);
+				else if ((block.getY() + newTranslateY + block.getHeight()) > gridLength){ // if going out of right end of grid
 					newTranslateY -= (block.getY() + newTranslateY + block.getHeight()) - gridLength;
 				}
-
-				// if ((newTranslateY + block.getHeight()) < 0){
-				// 	System.out.println(ANSI_BLUE + "..................." + ANSI_RESET);
-				// 	//newTranslateY = block.getHeight();
-				// }
 				((Group)t.getSource()).getChildren().get(0).setTranslateY(newTranslateY);
 			}
 
 
 		}
 	};
-
-	// private void moveHorizontal(MouseEvent t){
-	// 	double offsetX = t.getSceneX() - orgSceneX;
-	// 	double newTranslateX = orgTranslateX + offsetX;
-	// 	t.getSource().setTranslateX(newTranslateX);
-	// }
 }
--- GUI/GridLock.java
+++ GUI/GridLock.java
@@ -11,15 +11,12 @@ public class GridLock extends Application {
 			Group root = new Group();
 			Grid grid = new Grid(6);
 
-			//Rectangle r = grid.createSprite(0,0,100,50);
-
 			root.getChildren().addAll(grid.getGridSquares());
-			//root.getChildren().add(r);
-			root.getChildren().addAll(grid.createSprite(0,0,100,50));
-			root.getChildren().addAll(grid.createSprite(200,50,100,50));
-			root.getChildren().addAll(grid.createSprite(50,200,100,50)); // this and above are horizontal blocks
-						// //System.out.println("----");
-			root.getChildren().addAll(grid.createSprite(200,100,50,100)); // this and below are vertical blocks
+			root.getChildren().addAll(grid.createSprite(1,1,98,48));
+			root.getChildren().addAll(grid.createSprite(201,51,98,48));
+			root.getChildren().addAll(grid.createSprite(51,201,98,48)); // this and above are horizontal blocks
+			//------------------------------------------------------------
+			root.getChildren().addAll(grid.createSprite(201,101,48,98)); // this and below are vertical blocks
 			// root.getChildren().addAll(grid.createSprite(0,0,50,100));
 			// root.getChildren().addAll(grid.createSprite(50,50,50,100));
 			// root.getChildren().addAll(grid.createSprite(100,100,50,100));
