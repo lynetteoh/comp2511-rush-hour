a0708782836c6e8b5293829bb2eeb8f2b673043e
ExcaliburXX <e.ngo@student.unsw.edu.au>
2018-05-13 04:00:12 +1000

    removed debugging and cleaned up

--- GUI/Grid.java
+++ GUI/Grid.java
@@ -29,11 +29,9 @@ public class Grid {
 	private double orgTranslateY;
 	private int gridLength;
 	private int sLength;
-	//private Group m_draggableNode;
 
-	// can change to one for loop . size*size, if i % size == 0, make xPos = 0, yPos += 50
+
 	public Grid(int size){
-		//Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		this.sLength = 50; // grid square side length
 		this.gridLength = size*sLength;
 		double xPos = 0;
@@ -54,29 +52,24 @@ public class Grid {
 	public Group createSprite(int xPos, int yPos, int w, int h){
 		System.out.println("Wat");
 		Group dragNode = new Group();
-		//double orgSceneX, orgSceneY;
 		Sprite s = new Sprite(xPos, yPos, w, h);
 		dragNode.getChildren().add(s);
-		dragNode.setOnMousePressed(rectOnMousePressedEventHandler);
-		dragNode.setOnMouseDragged(rectOnMouseDraggedEventHandler);
-		dragNode.setOnMouseReleased(rectOnMouseReleasedEventHandler);
+		dragNode.setOnMousePressed(OnMousePressedEventHandler);
+		dragNode.setOnMouseDragged(OnMouseDraggedEventHandler);
+		dragNode.setOnMouseReleased(OnMouseReleasedEventHandler);
 		this.blocks.add(s);
 		return dragNode;
-		// ImageView i = new ImageView(s.getSprite());
-		// return i; - might have forums for custom objects added scene like this one
 	}
 
 	public ArrayList<Rectangle> getGridSquares(){
 		return this.gridSquares;
 	}
 
-	EventHandler<MouseEvent> rectOnMousePressedEventHandler =
+	EventHandler<MouseEvent> OnMousePressedEventHandler =
 		new EventHandler<MouseEvent>() {
 
 		@Override
 		public void handle(MouseEvent t) {
-			// System.out.println(gridLength);
-			//System.out.println(t.getSource().getClass().getName() + " | " + ((Group)t.getSource()).getChildren().get(0).getTranslateX());
 			orgSceneX = t.getSceneX();
 			orgSceneY = t.getSceneY();
 			orgTranslateX = ((Group)t.getSource()).getChildren().get(0).getTranslateX();
@@ -84,12 +77,11 @@ public class Grid {
 		}
 	};
 
-	EventHandler<MouseEvent> rectOnMouseDraggedEventHandler =
+	EventHandler<MouseEvent> OnMouseDraggedEventHandler =
 		new EventHandler<MouseEvent>() {
 
 		@Override
 		public void handle(MouseEvent t) {
-			//System.out.println("gridLength" + gridLength);
 			Sprite block = ((Sprite)((Group)t.getSource()).getChildren().get(0));
 			if (block.getOrientation().equals("HORIZONTAL")){
 
@@ -125,7 +117,6 @@ public class Grid {
 					}
 				}
 				else if ((block.getY() + newTranslateY + block.getHeight()) > gridLength){ // if going out of right end of grid
-					//System.out.println(ANSI_GREEN + "..................." + ANSI_RESET);
 					newTranslateY -= (block.getY() + newTranslateY + block.getHeight()) - gridLength;
 				}
 				((Group)t.getSource()).getChildren().get(0).setTranslateY(newTranslateY);
@@ -135,7 +126,7 @@ public class Grid {
 		}
 	};
 
-	EventHandler<MouseEvent> rectOnMouseReleasedEventHandler =
+	EventHandler<MouseEvent> OnMouseReleasedEventHandler =
 		new EventHandler<MouseEvent>() {
 
 		@Override
--- GUI/Sprite.java
+++ GUI/Sprite.java
@@ -6,22 +6,13 @@ import javafx.scene.Cursor;
 import javafx.scene.input.MouseEvent;
 
 public class Sprite extends Rectangle {
-	//Rectangle sprite;
 	private String orientation = "";
 
 	public Sprite(int xPos, int yPos, int width, int height){
-	    //this.sprite = new Rectangle(xPos, yPos, width, height);
 		super(xPos, yPos, width, height);
 		super.setArcHeight(5);
 		super.setArcWidth(5);
-		//super.setFill(Color.BLUE);
 		super.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-		//super.setStroke(Color.BLUE);
-
-		// this.sprite.setArcHeight(5);
-		// this.sprite.setArcWidth(5);
-		// this.sprite.setFill(Color.BLUE);
-		// this.sprite.setStroke(Color.BLUE);
 		System.out.println("width: " + width + " | height: " + height);
 		if (width > height){
 			orientation += "HORIZONTAL";
@@ -32,10 +23,6 @@ public class Sprite extends Rectangle {
 			System.out.println(">> V");
 		}
 	}
-
-	// public Rectangle getSprite(){
-	// 	return this.sprite;
-	// }
 
 	public String getOrientation(){
 		return this.orientation;
