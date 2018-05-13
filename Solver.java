d9a8e4bcf767fa76b05d24cba0c04b1e2eb884b1
jysim3 <junyang.0607@gmail.com>
2018-05-13 22:39:04 +1000

    Solver with 'easy' heuristic

--- Solver.java
+++ Solver.java
@@ -1,151 +1,101 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class Solver {
 
-	private Board startingBoard;
-	private ArrayList<Move> moves;
-	
-	public Solver(Board starting)
-	{
-		this.startingBoard = starting;
-		this.moves = new ArrayList<Move>();
-	}
-	
-	public boolean Solve()
-	{
-		Queue<BoardState> queue = new LinkedList<BoardState>();
-		
-		ArrayList<int[][]> closedSet = new ArrayList<int[][]>();
-		
-		int moves = 0;
-		
-		BoardState start = new BoardState(startingBoard.getMatrix(), startingBoard.getVehiclesList(), moves, null);
-		
-		queue.add(start);
-		
-		while(queue.size() > 0)
-		{
-			//System.out.println("Dequeue");
-			BoardState current = queue.poll();
-			//current.getBoard().printBoard();
-			
-			for(Vehicle v : current.getBoard().getVehiclesList())
-			{
-				//System.out.println("Vehicle: " + v.getId());
-				if(current.getBoard().canMoveForward(v) > 0)
-				{
-					//System.out.println("Can move forward");
-					current.getBoard().moveForward(v);
-					Board b = current.getBoard();
-					
-					//System.out.println("\tPrint board: ");
-					//b.printBoard();
-					//System.out.println();
-					
-					int[][] B = matrixCopy(b.getMatrix());
-					BoardState bs = new BoardState(B, b.getVehiclesList(), moves++, current);
-					
-					if(!seenAlready(bs.getBoard().getMatrix(), closedSet))
-					{
-						closedSet.add(bs.getBoard().getMatrix());
-						
-						//System.out.println("\t\t\t\tAdd to Queue, Print board: ");
-						//b.printBoard();
-						
-						queue.add(bs);
-					}
-					current.getBoard().moveBackward(v);
-					if(Solved(b.getMatrix()))
-					{
-						b.printBoard();
-						return true;
-					}
-						
-				}
-				if(current.getBoard().canMoveBackward(v) > 0)
-				{
-					//System.out.println("Can move backward");
-					current.getBoard().moveForward(v);
-					Board a = current.getBoard();
-					
-					//System.out.println("\tPrint board: ");
-					//a.printBoard();
-					//System.out.println();
-					int[][] A = matrixCopy(a.getMatrix());
-					BoardState as = new BoardState(A, a.getVehiclesList(), moves++, current);
-					
-					if(!seenAlready(as.getBoard().getMatrix(), closedSet))
-					{
-						closedSet.add(as.getBoard().getMatrix());
-						
-						//System.out.println("\t\t\t\tAdd to Queue, Print board: ");
-						//a.printBoard();
-						
-						queue.add(as);
-					}
-					current.getBoard().moveBackward(v);
-					if(Solved(a.getMatrix()))
-					{
-						a.printBoard();
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
-	
-	private boolean seenAlready(int[][] matrix, ArrayList<int[][]> matrices)
-	{
-		if(matrices.size() > 0)
-		{
-			for(int i = 0; i < matrices.size(); i++) {
-				for(int j = 0; j < matrices.get(i).length; j++)
-				{
-					//System.out.println("Closed set size: " + matrices.size());
-					if(!Arrays.equals(matrices.get(i)[j], matrix[j]))
-					{
-						//System.out.println("no match");
-						return false;
-					}
-				}
-			}
-			return true;
-		}
-		
-		return false;
-	}
-	
-	private int[][] matrixCopy(int[][] original)
-	{
-		int[][] copy = new int[original.length][original.length];
-		for(int i = 0; i < original.length; i++)
-		{
-			for(int j = 0; j < original.length; j++)
-			{
-				copy[i][j] = original[i][j];
-			}
-		}
-		return copy;
-	}
-	
-	private boolean Solved(int[][] matrix)
-	{
-		if(matrix[2][5] == 1)		// position of door
-		{
-			return true;
-		}
-		if(matrix[2][4] == 1 && matrix[2][5] == 0)
-		{
-			return true;
-		}
-		if(matrix[2][3] == 1 && matrix[2][4] == 0 && matrix[2][5] == 0)
-		{
-			return true;
-		}
-		return false;
-	}
+public class Solver  {
+    private Board board;
+    private SolverMethod solver;
+    
+    public Solver (Board b) {
+        this.board = b;
+        this.solver = new solverEasy();
+    }
+
+    private boolean solved(Board b)
+    {
+    	int [][] matrix = b.getMatrix();
+        if(matrix[2][5] == 1)       // position of door
+        {
+            return true;
+        }
+        if(matrix[2][4] == 1 && matrix[2][5] == 0)
+        {
+            return true;
+        }
+        if(matrix[2][3] == 1 && matrix[2][4] == 0 && matrix[2][5] == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public String solve() {
+        PriorityQueue<Board> q = new PriorityQueue<Board>(10, solver);
+        HashMap<String, Move> previousMoves = new HashMap<String, Move>();
+        q.add(board);
+        previousMoves.put(board.toString(), new Move(null,0,board));
+        Board b;
+        while (!q.isEmpty()) {
+            b = q.remove();
+
+
+           Move m = previousMoves.get(b.toString());
+            if (solved(b)) {
+            	print(m.previousBoard);
+            	ArrayList<Move> moves = new ArrayList<Move>();
+            	while (m.previousBoard != board) {
+            		moves.add(m);
+            		m = previousMoves.get(m.previousBoard.toString());
+            	}
+            	moves.add(m);
+            	for (Move x : moves) {
+            		print(x.previousBoard);
+            	}
+            	
+            	b.toString();
+            	return b.toString();
+            }
+
+            for (Vehicle v: b.getVehiclesList()) {
+                if (b.moveForward(v)) {
+                    if (previousMoves.containsKey(b.toString()) && board != b) {
+                    	b.moveBackward(v);
+                    } else {
+                    	
+	                    Board newBoard = new Board(b);
+	                    b.moveBackward(v);
+	                    Move newMoves = new Move(v,1, b);
+	                    if (solver.compare(b,newBoard) >= 0) {
+		                    q.add(newBoard);
+		                    previousMoves.put(newBoard.toString(), newMoves);
+	                    }
+                    }
+                } 
+                if (b.moveBackward(v)) {
+                    if (previousMoves.containsKey(b.toString()) && board != b) {
+                    	b.moveForward(v);
+                    	continue;
+                    } 
+                    Board newBoard = new Board(b);
+                    b.moveForward(v);
+                    Move newMoves = new Move(v,1, b);
+                    if (solver.compare(b,newBoard) >= 0) {
+	                    q.add(newBoard);
+	                    previousMoves.put(newBoard.toString(), newMoves);
+                    }
+                }
+                
+            }
+        }
+        return null;
+    }            
+
+    private void print(Object x) {
+    	System.out.println(x.toString());
+    }
+
+
 }
