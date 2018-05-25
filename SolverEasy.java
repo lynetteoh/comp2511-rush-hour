80cf368ea5a597d740d0a86a4f1f903deb39dbe3
lynetteoh <leanlynn.oh@student.unsw.edu.au>
2018-05-25 10:54:31 +1000

    change the structure of the file and added the script from sample file

--- SolverEasy.java
+++
@@ -1,6 +0,0 @@
-
-public class SolverEasy implements SolverMethod {
-    public int compare(Board b1, Board b2) {
-        return b1.getnMoves() - b2.getnMoves();
-    }
-}
--- run.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/sh
- 
- javac GridLock.java
- java GridLock
