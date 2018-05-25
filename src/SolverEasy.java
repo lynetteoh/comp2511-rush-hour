10cab8675ce67dc7c362c74c32b5aa29e3f3c494
NessieNono <30561974+NessieNono@users.noreply.github.com>
2018-05-25 19:38:50 +1000

    Added Javadoc

--- src/SolverEasy.java
+++ src/SolverEasy.java
@@ -1,4 +1,6 @@
-
+/**
+* A Class which implements solver method. Compares 2 boards based on many moves it takes to solve the boards.
+*/
 public class SolverEasy implements SolverMethod {
     public int compare(Board b1, Board b2) {
         return b1.getnMoves() - b2.getnMoves();
